abd082edb4d698f32e7cb4fc1d757afcc4a02cd1
Tin Long LEUNG <tlleungac@connect.ust.hk>
2019-10-29 22:44:58 +0800

    Implemented some functions

--- src/main/java/project/Arena.java
+++ src/main/java/project/Arena.java
@@ -23,7 +23,17 @@ public class Arena {
     private LinkedList<BasicTower> towers = new LinkedList<>();
     private LinkedList<Projectile> projectiles = new LinkedList<>();
     private LinkedList<Monster> monsters = new LinkedList<>();
-    
+
+    /**
+     * Finds the grid position corresponding to a specified pixel.
+     * @param x The x-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
+     * @param y The y-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
+     * @return An int array that contains the x- and y- position of the grid at indices 0 and 1 respectively.
+     * @see Coordinates
+     */
+    private int[] getGrid(int x, int y) {
+        return new int[] { x / UIController.GRID_WIDTH, y / UIController.GRID_HEIGHT };
+    }
 
     /**
      * The default constructor of the Arena class.
@@ -33,27 +43,107 @@ public class Arena {
     }
 
     /**
+     * An enum for filtering objects in the Arena according to type.
+     */
+    public enum TypeFilter { Tower, Projectile, Monster }
+
+    /**
      * Finds all objects that are located at a specified pixel.
      * @param x The x-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
      * @param y The y-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
-     * @return A linked list containing all objects that satisfy the above criteria.
+     * @param filter Only the types that are specified will be included in the result.
+     * @return A linked list containing a reference to each object that satisfies the above criteria.
      * @see Coordinates
+     * @see TypeFilter
      */
-    public LinkedList<Object> getObjectsAtPixel(int x, int y)
+    public LinkedList<Object> getObjectsAtPixel(int x, int y, EnumSet<TypeFilter> filter)
     {
-        throw new NotImplementedException("TODO");
+        LinkedList<Object> list = new LinkedList<>();
+
+        if (filter.contains(TypeFilter.Tower)) {
+            for (BasicTower t : towers) {
+                Coordinates c = t.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(t);
+            }
+        }
+        
+        if (filter.contains(TypeFilter.Projectile)) {
+            for (Projectile p : projectiles) {
+                Coordinates c = p.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(p);
+            }
+    
+        }
+
+        if (filter.contains(TypeFilter.Monster)) {
+            for (Monster m : monsters) {
+                Coordinates c = m.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(m);
+            }
+        }
+
+        return list;
     }
 
     /**
      * Finds all objects that are located inside the grid where a specified pixel is located.
      * @param x The x-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
      * @param y The y-coordinate of the pixel, as defined in {@link Coordinates#Coordinates()}.
-     * @return A linked list containing all objects that satisfy the above criteria.
+     * @param filter Only the types that are specified will be included in the result.
+     * @return A linked list containing a reference to each object that satisfies the above criteria.
      * @see Coordinates
+     * @see TypeFilter
      */
-    public LinkedList<Object> getObjectsInGrid(int x, int y)
+    public LinkedList<Object> getObjectsInGrid(int x, int y, EnumSet<TypeFilter> filter)
     {
-        throw new NotImplementedException("TODO");
+        LinkedList<Object> list = new LinkedList<>();
+
+        if (filter.contains(TypeFilter.Tower)) {
+            for (BasicTower t : towers) {
+                Coordinates c = t.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(t);
+                else {
+                    int[] gridPosition = getGrid(c.getX(), c.getY());
+    
+                    int xMin = gridPosition[0] * UIController.GRID_WIDTH;
+                    int xMax = xMin + UIController.GRID_WIDTH;
+                    int yMin = gridPosition[1] * UIController.GRID_HEIGHT;
+                    int yMax = yMin + UIController.GRID_HEIGHT;
+    
+                    if (xMin <= x && x <= xMax && yMin <= y && y <= yMax)
+                        list.add(t);
+                }
+            }
+        }
+        
+        if (filter.contains(TypeFilter.Projectile)) {
+            for (Projectile p : projectiles) {
+                Coordinates c = p.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(p);
+            }
+    
+        }
+
+        if (filter.contains(TypeFilter.Monster)) {
+            for (Monster m : monsters) {
+                Coordinates c = m.getCoordinates();
+    
+                if (c.getX() == x && c.getY() == y)
+                    list.add(m);
+            }
+        }
+
+        return list;
     }
 
     /**
@@ -65,7 +155,7 @@ public class Arena {
      */
     public boolean canBuildTower(int x, int y)
     {
-        throw new NotImplementedException("TODO");
+        return getObjectsInGrid(x, y, EnumSet.of(TypeFilter.Tower, TypeFilter.Monster)).isEmpty();
     }
 
     /**
@@ -81,12 +171,12 @@ public class Arena {
 
     /**
      * Destroys the specified Tower.
-     * @param Tower The Tower to be destroyed.
+     * @param tower The Tower to be destroyed.
      */
-    // public void destroyTower(Tower tower)
-    // {
-    //      towers.remove(tower);
-    // }
+    public void destroyTower(BasicTower tower)
+    {
+        towers.remove(tower);
+    }
 
     /**
      * Creates a Projectile at a specified pixel.
@@ -101,7 +191,7 @@ public class Arena {
 
     /**
      * Removes the specified Projectile from the arena.
-     * @param Projectile The Projectile to be removed.
+     * @param projectile The Projectile to be removed.
      */
     public void removeProjectile(Projectile projectile)
     {
