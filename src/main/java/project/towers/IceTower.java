00a36042accbc7da6c2edec3c562474ad2385e86
jshian <jshian@connect.ust.hk>
2019-11-01 17:45:40 +0800

    finish IceTower

--- src/main/java/project/towers/IceTower.java
+++ src/main/java/project/towers/IceTower.java
@@ -1,10 +1,11 @@
 package project.towers;
 
 import project.*;
+import project.monsters.Monster;
 
 public class IceTower extends Tower{
     // State
-    private int slowDown;
+    private double slowDown;
 
     /**
      * Constructor for IceTower class.
@@ -12,16 +13,23 @@ public class IceTower extends Tower{
      */
     public IceTower(Arena arena){
         super(arena);
-        super.buildingCost=20;
+        this.attackPower = 5;
+        this.buildingCost = 15;
+        this.shootingRange = 50;
         this.slowDown = 10;
     }
 
     @Override
     public boolean upgrade(int resource){
         if(resource >= 20){
-            slowDown+=5;
+            this.slowDown+=5;
+            this.attackPower+=5;
             return true;
         }
         return false;
+    }
+
+    public void slowDown(Monster monster){
+        monster.setSpeed(monster.getSpeed()-this.slowDown);
     }
 }
