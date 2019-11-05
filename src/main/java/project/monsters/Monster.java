7f626791fd1f1a38c532e2d20922a6dbed347d6a
cshoae <cshoae@connect.ust.hk>
2019-11-05 22:47:46 +0800

    minor javadoc correction

--- src/main/java/project/monsters/Monster.java
+++ src/main/java/project/monsters/Monster.java
@@ -89,7 +89,7 @@ public abstract class Monster implements MovesInArena, Comparable<Monster> {
      * @param difficulty The difficulty of the monster.
      * @param start The starting location of the monster.
      * @param destination The destination of the monster. It will try to move there.
-     * @param imageView The image of the monster.
+     * @param imageView The imageView of the monster.
      */
     public Monster(double difficulty, @NonNull Coordinates start, @NonNull Coordinates destination, @NonNull ImageView imageView) {
         this.coordinates = start;
