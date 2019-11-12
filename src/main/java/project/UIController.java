a025cd9893bb333dac3fb5117e7310c6f6e1567e
cshoae <cshoae@connect.ust.hk>
2019-11-12 17:01:46 +0800

    remove vbox when click destroy

--- src/main/java/project/UIController.java
+++ src/main/java/project/UIController.java
@@ -422,6 +422,7 @@ public class UIController {
         });
         destroyBtn.setOnAction(e2 -> {
             arena.removeObject(t);
+            paneArena.getChildren().remove(vb);
         });
 
         paneArena.getChildren().add(vb);
