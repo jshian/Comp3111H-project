ef8beee0862141ba3e34c1c45ff2e3701569f77e
Tin Long LEUNG <tlleungac@connect.ust.hk>
2019-11-13 15:58:49 +0800

    Fixed issue of app not starting

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,26 +1,7 @@
 package project;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/project.fxml"));
-        Parent root = loader.load();
-        primaryStage.setTitle("Tower Defence");
-        primaryStage.setScene(new Scene(root, 600, 480));
-        primaryStage.show();
-        UIController appController = (UIController)loader.getController();
-        appController.createArena();
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        MainApplication.main(args);
     }
 }
