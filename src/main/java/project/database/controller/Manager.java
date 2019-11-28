91929d60de06c61b71995cd287816e6881fbdb7b
cshoae <cshoae@connect.ust.hk>
2019-11-28 22:17:14 +0800

    minor correction

--- src/main/java/project/database/controller/Manager.java
+++ src/main/java/project/database/controller/Manager.java
@@ -71,6 +71,7 @@ public class Manager {
      */
     public static void save(ArenaInstance arenaInstance) {
         if(entityManagerFactory == null) return;
+        removeAll();
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction tran = null;
         try {
